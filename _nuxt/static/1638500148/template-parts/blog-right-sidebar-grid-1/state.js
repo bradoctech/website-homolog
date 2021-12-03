1e4ab7d3dfcc3c116dd30b69b51b3b92e17eec27
oxymus <oxymus@gmail.com>
2021-12-02 23:09:47 -0400

    ID-27 - Alterando router base para '/'

---
+++ _nuxt/static/1638500148/template-parts/blog-right-sidebar-grid-1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638500148",layout:"Blog",error:a,serverRendered:true,routePath:"\u002Ftemplate-parts\u002Fblog-right-sidebar-grid-1",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
