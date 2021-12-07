7b14294fdca573d2792362f1e8a86f5650f75d5f
oxymus <oxymus@gmail.com>
2021-12-06 23:03:13 -0400

    ID-30 - Atualizando homolog

---
+++ _nuxt/static/1638845982/template-parts/about-1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwebsite-homolog\u002F_nuxt\u002Fstatic\u002F1638845982",layout:"AboutUs",error:a,serverRendered:true,routePath:"\u002Ftemplate-parts\u002Fabout-1",config:{_app:{basePath:"\u002Fwebsite-homolog\u002F",assetsPath:"\u002Fwebsite-homolog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
