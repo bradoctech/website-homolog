0ae5e4a0344a1028975a659954aeb32e2bc5d921
oxymus <oxymus@gmail.com>
2021-12-13 17:32:27 -0400

    Deploy 13-12-21

---
+++ _nuxt/static/1639429595/xamin-vue/about-1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwebsite-homolog\u002F_nuxt\u002Fstatic\u002F1639429595",layout:"LandingPage1",error:a,serverRendered:true,routePath:"\u002Fxamin-vue\u002Fabout-1",config:{_app:{basePath:"\u002Fwebsite-homolog\u002F",assetsPath:"\u002Fwebsite-homolog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
