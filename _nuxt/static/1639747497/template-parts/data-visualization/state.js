8b79b676dd0e20ba1d5319ec7daa8f3d875ab693
oxymus <oxymus@gmail.com>
2021-12-17 09:31:52 -0400

    Finalizando ajustes metas ogs

---
+++ _nuxt/static/1639747497/template-parts/data-visualization/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwebsite-homolog\u002F_nuxt\u002Fstatic\u002F1639747497",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftemplate-parts\u002Fdata-visualization",config:{_app:{basePath:"\u002Fwebsite-homolog\u002F",assetsPath:"\u002Fwebsite-homolog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
