6d8b81d4ec57580cd76a9fe543ec2cb955f0622d
oxymus <oxymus@gmail.com>
2021-12-23 15:22:59 -0400

    Correções de links

--- _nuxt/static/1640277610/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwebsite-homolog\u002F_nuxt\u002Fstatic\u002F1640277610",layout:"default",error:a,serverRendered:false,routePath:"\u002F",config:{_app:{basePath:"\u002Fwebsite-homolog\u002F",assetsPath:"\u002Fwebsite-homolog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1640287040/template-parts/about-2/about-2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwebsite-homolog\u002F_nuxt\u002Fstatic\u002F1640287040",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftemplate-parts\u002Fabout-2\u002Fabout-2",config:{_app:{basePath:"\u002Fwebsite-homolog\u002F",assetsPath:"\u002Fwebsite-homolog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
