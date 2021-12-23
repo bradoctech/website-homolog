be35134c0b1530d25b12e0e6b9a6a272b3ca6709
oxymus <oxymus@gmail.com>
2021-12-23 12:42:17 -0400

    Testando ajustes no nuxt config - 1

--- _nuxt/static/1640266668/template-parts/blog-two-column-grid/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/template-parts/blog-two-column-grid", {data:[{}],fetch:{},mutations:void 0});
--- _nuxt/static/1640266668/template-parts/blog/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwebsite-homolog\u002F_nuxt\u002Fstatic\u002F1640266668",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftemplate-parts\u002Fblog",config:{_app:{basePath:"\u002Fwebsite-homolog\u002F",assetsPath:"\u002Fwebsite-homolog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
