0e8e7a0423c88683d140f74eb3cd2bc1f23bd741
oxymus <oxymus@gmail.com>
2022-01-13 19:25:06 -0400

    Corrigindo preloader dos slides na home que estava em branco

---
+++ _nuxt/static/1642115871/template-parts/masonry-2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwebsite-homolog\u002F_nuxt\u002Fstatic\u002F1642115871",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftemplate-parts\u002Fmasonry-2",config:{_app:{basePath:"\u002Fwebsite-homolog\u002F",assetsPath:"\u002Fwebsite-homolog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
