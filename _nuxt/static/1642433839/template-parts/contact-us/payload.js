f00cfaf38e4fa1900bd1e602f6642836f1cea23d
oxymus <oxymus@gmail.com>
2022-01-17 11:57:24 -0400

    Atualizando homolog - ID-59 / ID-64 / ID-65

---
+++ _nuxt/static/1642433839/template-parts/contact-us/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/template-parts/contact-us", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1642433839/template-parts/landing-page-11/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/template-parts/landing-page-11", {data:[{}],fetch:{},mutations:void 0});
