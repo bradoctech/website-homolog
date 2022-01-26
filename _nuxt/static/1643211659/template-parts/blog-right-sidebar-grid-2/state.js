80d29f8b33f19ef3ed5b5c0142fd22efc16c4e2d
oxymus <oxymus@gmail.com>
2022-01-26 11:49:13 -0400

    Atualizando homolog - ID-83 e ID-90

---
+++ _nuxt/static/1643211659/template-parts/blog-right-sidebar-grid-2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwebsite-homolog\u002F_nuxt\u002Fstatic\u002F1643211659",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftemplate-parts\u002Fblog-right-sidebar-grid-2",config:{_app:{basePath:"\u002Fwebsite-homolog\u002F",assetsPath:"\u002Fwebsite-homolog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
