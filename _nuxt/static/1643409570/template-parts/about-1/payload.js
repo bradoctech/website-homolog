6c058333451bc6f85c31eb4d604a0796c8302e0a
oxymus <oxymus@gmail.com>
2022-01-28 19:15:48 -0400

    Atualizando homolog - ID-63

---
+++ _nuxt/static/1643409570/template-parts/about-1/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/template-parts/about-1", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1643409570/template-parts/data-management/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwebsite-homolog\u002F_nuxt\u002Fstatic\u002F1643409570",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftemplate-parts\u002Fdata-management",config:{_app:{basePath:"\u002Fwebsite-homolog\u002F",assetsPath:"\u002Fwebsite-homolog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
