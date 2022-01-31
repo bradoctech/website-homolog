283f6ab2d547faf3c68f31a09cc8ffd3de2d1bff
oxymus <oxymus@gmail.com>
2022-01-31 19:25:49 -0400

    Atualizando homolog - ID-78

---
+++ _nuxt/static/1643671053/template-parts/landing-page-19/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwebsite-homolog\u002F_nuxt\u002Fstatic\u002F1643671053",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftemplate-parts\u002Flanding-page-19",config:{_app:{basePath:"\u002Fwebsite-homolog\u002F",assetsPath:"\u002Fwebsite-homolog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
