ddc391c93df6d610033ae61e126aebc23768a63c
Lucas Lima <lucasfelipeflima@gmail.com>
2022-04-20 10:35:30 -0400

    subindo alterações pra homolog

---
+++ _nuxt/static/1650465139/servicos/data-science/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/servicos/data-science", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1650465139/template-parts/portfolio-no-space/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/template-parts/portfolio-no-space", {data:[{}],fetch:{},mutations:void 0});
