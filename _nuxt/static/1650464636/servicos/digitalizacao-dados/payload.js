b5b0359cfa81174d3260258be8450218b2c950e6
Lucas Lima <lucasfelipeflima@gmail.com>
2022-04-20 10:27:17 -0400

    subindo alterações pra homolog

---
+++ _nuxt/static/1650464636/servicos/digitalizacao-dados/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/servicos/digitalizacao-dados", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1650464636/template-parts/blog-right-sidebar-grid-2/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/template-parts/blog-right-sidebar-grid-2", {data:[{}],fetch:{},mutations:void 0});
